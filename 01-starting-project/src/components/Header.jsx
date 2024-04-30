413ad5e423b29eb558807d01dbbd26f5cd2118c8
Mustapha <mustaphabakare2@gmail.com>
2024-04-30 17:52:28 +0100

    new

---
+++ 01-starting-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import logoImg from "../assets/quiz-logo.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logoImg} alt="Logo Image" />
+      <h1>ReactQuiz</h1>
+    </header>
+  );
+}
